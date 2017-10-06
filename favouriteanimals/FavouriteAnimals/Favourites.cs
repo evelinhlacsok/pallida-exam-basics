0cef84df9fc23af04c6edc6eef363669691ccedc
evi <evelinhlacsok@gmail.com>
2017-10-06 11:09:52 +0200

    new class and printUsage created

---
+++ favouriteanimals/FavouriteAnimals/Favourites.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavouriteAnimals
+{
+    class Favourites
+    {
+        public void PrintUsage()
+        {
+            Console.WriteLine("```C# FavouriteAnimals [animal] [animal]```");
+        }
+    }
+}
